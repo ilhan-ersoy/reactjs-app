296127c8c904f5a52d289d03a3b3cca9a1e6b69c
ilhan-ersoy <ilhan.ersoy.ie@gmail.com>
2021-09-16 22:15:15 +0300

    first commit.

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+
+import useFetch from "./UseFetch";
+import BlogList from "./BlogList";
+
+
+const Home = () => {
+    const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
+    return (
+        <div className="home">
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={blogs} /> }
+        </div>
+    );
+}
+
+export default Home;
